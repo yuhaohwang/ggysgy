c0abf61506455c0e7f47b4526286544788bcc215
hao7680 <yuhao_hwang@foxmail.com>
2022-06-29 19:47:29 +0800

    修复了空白页高度问题

--- components/use-empty/use-empty.vue
+++ components/use-empty/use-empty.vue
@@ -9,102 +9,102 @@
 </template>
 
 <script>
-  export default {
-    props: {
-      eStyle: {
-        type: String,
-        default: 'square',
+export default {
+  props: {
+    eStyle: {
+      type: String,
+      default: 'square',
+    },
+    eType: {
+      type: String,
+      default: 'other',
+    },
+    tip: {
+      type: String,
+      default: '暂无数据',
+    },
+    btnTip: {
+      type: String,
+      default: '去逛逛',
+    },
+    btnGoto: {
+      type: String,
+      default: '/pages/tabbar/home',
+    },
+    auto: {
+      type: Boolean,
+      default: !0,
+    },
+    height: {
+      type: String,
+      default: '100%',
+    },
+  },
+  computed: {
+    dtype: {
+      get() {
+        console.log('get dtype', arguments)
+        let imgobj = this.imgs.find(x => x.type == this.eType)
+        this.imgurl = imgobj ? imgobj.url : this.imgurl_dft
+        return this.eType
       },
-      eType: {
-        type: String,
-        default: 'other',
-      },
-      tip: {
-        type: String,
-        default: '暂无数据',
-      },
-      btnTip: {
-        type: String,
-        default: '去逛逛',
-      },
-      btnGoto: {
-        type: String,
-        default: '/pages/tabbar/home',
-      },
-      auto: {
-        type: Boolean,
-        default: !0,
-      },
-      height: {
-        type: String,
-        default: '100vh',
+      set(val) {
+        console.log('set dtype', arguments)
       },
     },
-    computed: {
-      dtype: {
-        get() {
-          console.log('get dtype', arguments)
-          let imgobj = this.imgs.find(x => x.type == this.eType)
-          this.imgurl = imgobj ? imgobj.url : this.imgurl_dft
-          return this.eType
-        },
-        set(val) {
-          console.log('set dtype', arguments)
-        },
-      },
-    },
-    data() {
-      return {
-        imgurl_dft: '/static/images/empty/empty.jpg',
-        imgurl: '',
-        imgs: [
-          { type: 'cart', url: '/static/images/empty/cart.jpg' },
-          { type: 'search', url: '/static/images/empty/search.jpg' },
-          { type: 'other', url: '/static/images/empty/empty.jpg' },
-        ],
+  },
+  data() {
+    return {
+      imgurl_dft: '/static/images/empty/empty.jpg',
+      imgurl: '',
+      imgs: [
+        { type: 'cart', url: '/static/images/empty/cart.jpg' },
+        { type: 'search', url: '/static/images/empty/search.jpg' },
+        { type: 'other', url: '/static/images/empty/empty.jpg' },
+      ],
+    }
+  },
+  methods: {
+    to() {
+      this.$emit('goto', {
+        type: 'goto',
+      })
+
+      if (this.auto) {
+        // 跳转指定页
+        uni.switchTab({
+          url: this.btnGoto,
+        })
       }
     },
-    methods: {
-      to() {
-        this.$emit('goto', {
-          type: 'goto',
-        })
-
-        if (this.auto) {
-          // 跳转指定页
-          uni.switchTab({
-            url: this.btnGoto,
-          })
-        }
-      },
-    },
-  }
+  },
+}
 </script>
 
 <style lang="scss">
-  .use-empty-container {
-    .use-empty {
-      .round {
-        padding: 25% 50rpx;
+.use-empty-container {
+  .use-empty {
+    .round {
+      padding: 25% 50rpx;
 
-        button {
-          width: 220rpx;
-        }
-      }
-
-      image {
-        width: 160rpx;
-        height: 160rpx;
-      }
-
-      .title {
-        color: #c0c0c0;
-      }
-
-      .use-btn {
-        display: inline-block;
-        font-size: $font-base + 2upx;
+      button {
+        width: 220rpx;
       }
     }
+
+    image {
+      width: 160rpx;
+      height: 160rpx;
+    }
+
+    .title {
+      color: #c0c0c0;
+    }
+
+    .use-btn {
+      display: inline-block;
+      font-size: $font-base + 2upx;
+    }
   }
+}
 </style>
